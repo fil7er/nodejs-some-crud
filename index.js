23629dfce7abe8869e149b072834360f6461f546
fil7er <fil7er@gmail.com>
2022-06-30 16:22:29 -0400

    Get image to print

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const fs = require('fs')
+const request = require('request')
+const path = require('path')
+var download = function(uri, filename, callback){
+    request.head(uri, function(err, res, body){    
+      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    });
+  };
+
+download('https://i.picsum.photos/id/372/1200/1200.jpg?hmac=p0huIhif1Pv0hf7lYYi0-Sbq3ITX9NpyPv7oc5oQOgw', 'file/test.jpeg', function(){
+    console.log('done');
+});
 
 app.get('/', (req, res) => {
-  res.json({
-    commit_id: process.env.COMMIT_ID || 'unknown',
-    port
-  })
+    res.sendFile(path.join(__dirname, '/app.html'));
 })
+
+app.get("/test.jpeg", (req, res) => {
+    res.sendFile(path.join(__dirname, "./file/test.jpeg"));
+  });
 
 app.listen(port, () => {
   console.log(`Server started in port ${port}`)
